eec98e94c360e63b2ff58003277f82c8dbd7a079
luisc <luis.grigore@alumnos.upm.es>
2024-10-02 17:22:18 +0200

    a

---
+++ eureka-server/src/main/java/com/fitness_social/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.fitness_social.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
