49276a38cc6fdc6ab099ef990f1c3b654e59bf53
luisc <luis.grigore@alumnos.upm.es>
2024-10-07 02:35:19 +0200

    User microservice created

--- user-microservice/src/main/java/com/example/user_microservice/UserService.java
+++ user-microservice/src/main/java/com/example/user_microservice/UserService.java
@@ -1,10 +1,13 @@
 package com.example.user_microservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements IUserService{
+    @Autowired
     private IUserRepos userRepos;
+    @Autowired
     private IUserDeleteHandler userDeleteHandler;
 
     @Override
