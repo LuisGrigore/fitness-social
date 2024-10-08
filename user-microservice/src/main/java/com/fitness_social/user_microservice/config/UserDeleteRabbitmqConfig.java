1b8740576ef1224eb976737d048f89e0532da0fa
luisc <luis.grigore@alumnos.upm.es>
2024-10-08 18:00:56 +0200

    Rabbitmq configured and integrated with user microservice

--- user-microservice/src/main/java/com/fitness_social/user_microservice/config/UserDeleteRabbitmqConfig.java
+++ user-microservice/src/main/java/com/fitness_social/user_microservice/config/UserDeleteRabbitmqConfig.java
@@ -1,9 +1,6 @@
 package com.fitness_social.user_microservice.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.support.converter.DefaultClassMapper;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
