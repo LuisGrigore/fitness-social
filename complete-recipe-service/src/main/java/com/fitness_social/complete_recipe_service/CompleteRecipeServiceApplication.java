d23bc09a61334c8e32b53b89663339daa6ce1521
luisc <luis.grigore@alumnos.upm.es>
2024-10-10 14:46:46 +0200

    Compleate recipe microservice created

---
+++ complete-recipe-service/src/main/java/com/fitness_social/complete_recipe_service/CompleteRecipeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitness_social.complete_recipe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteRecipeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteRecipeServiceApplication.class, args);
+	}
+
+}
---
+++ complete-recipe-service/src/test/java/com/fitness_social/complete_recipe_service/CompleteRecipeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness_social.complete_recipe_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteRecipeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
